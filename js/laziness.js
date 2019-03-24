4dae1218254420667e3171a350f7250bf13f7a74
Andrew A. Martinez <andrew.a.martinez33@gmail.com>
2019-03-24 16:19:52 -0700

    updated script to check if src attribute and data-src attribute are equal to each other before checking location of the element and setting the new src attribute.

--- js/laziness.js
+++ js/laziness.js
@@ -7,13 +7,16 @@ function laziness() {
     // return object with the height and width of the element, along with its distance from the top, bottom, right, and left.
     const distances = element.getBoundingClientRect();
     const imagePath = element.getAttribute('data-src');
+    const imageSrc = element.getAttribute('src');
 
     //if the element is within the boundaries of the viewport
-    if (distances.top >= 0 &&
-      distances.top < (window.innerHeight || document.documentElement.clientHeight) &&
-      distances.left >= 0 &&
-      distances.left < (window.innerWidth || document.documentElement.clientWidth)) {
-      element.setAttribute('src', imagePath);
+    if (imageSrc !== imagePath) {
+      if (distances.top >= 0 &&
+        distances.top < (window.innerHeight || document.documentElement.clientHeight) &&
+        distances.left >= 0 &&
+        distances.left < (window.innerWidth || document.documentElement.clientWidth)) {
+        element.setAttribute('src', imagePath);
+      }
     }
   })
 }
