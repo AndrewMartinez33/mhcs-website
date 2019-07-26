26e5208ad7471b11ca953e7ab06257e7125de415
Andrew A. Martinez <andrew.a.martinez33@gmail.com>
2019-07-25 18:30:15 -0700

    updates

--- js/gallery.js
+++ js/gallery.js
@@ -1,18 +1,19 @@
 function gallery(photosArray) {
   let photos = photosArray;
-  const imagesContainer = document.querySelector('#photos');
+  const imagesContainer = document.querySelector("#photos");
 
   photos.forEach(element => {
-    const img = document.createElement('img');
-    img.setAttribute('class', 'lazy');
-    img.setAttribute('src', '../images/gallery-images/lazy-background.svg');
-    img.setAttribute('data-src', `https://drewskidev.com/admin${element.path}`);
-    img.setAttribute('alt', `${element.meta.asset}`)
+    const img = document.createElement("img");
+    img.setAttribute("class", "lazy");
+    img.setAttribute("src", "../images/gallery-images/lazy-background.svg");
+    img.setAttribute("data-src", `https://drewskidev.com/admin${element.path}`);
+    img.setAttribute("alt", `${element.meta.asset}`);
     imagesContainer.appendChild(img);
   });
-
 }
 
-fetch('https://www.admin.drewskidev.com/api/collections/get/GalleryMyHCS?token=60f989d9826aab5d738a7ebac190a4')
+fetch(
+  "https://www.admin.drewskidev.com/api/collections/get/GalleryMyHCS?token=894a9d878341a1d354f1b8682343bf"
+)
   .then(res => res.json())
   .then(res => gallery(res.entries[0].GalleryMyHCS));
---
+++ js/topBanner.js
@@ -0,0 +1,7 @@
+const bannerMessage = {
+  message: "Learn about our NEW Permanent Makeup & Microblading Services"
+};
+
+const banner = document.querySelector("#top-banner");
+
+banner.textContent = bannerMessage.message;
